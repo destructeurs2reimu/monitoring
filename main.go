a216e5d4392bbfbdc99ebdb7c65ec3b015d23373
swtchcoder <janin.maxens@gmail.com>
2025-07-01 15:04:37 +0200

    Added basic bot

--- main.go
+++ main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/joho/godotenv"
+	"github.com/bwmarrin/discordgo"
 )
 
-func main() {
+var s *discordgo.Session
+
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
 
-	foo := os.Getenv("FOO")
-	log.Println("FOO=" + foo)
+func init() {
+	var err error
+
+	token := os.Getenv("TOKEN")
+	s, err = discordgo.New(token)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	err := s.Open()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("Logged in as %s (%s)\n", s.State.User.DisplayName(), s.State.User.ID)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
 }
